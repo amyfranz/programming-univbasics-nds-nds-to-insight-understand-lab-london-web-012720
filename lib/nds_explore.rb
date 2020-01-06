8476ab10242cb0a3e6927521621e7a5085ec65c6
Amy Franz <amy.franz@gmail.com>
2020-01-06 09:06:41 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,11 +9,13 @@ end
 def print_first_directors_movie_titles
   row_index = 0 
   while row_index < directors_database.length do
-    puts "Row #{row_index} has #{directors_database[row_index]} columns"
+    # puts "Row #{row_index} has #{directors_database[row_index]} columns"
     column_index = 0 
     while column_index < directors_database[row_index].length do
       coord = "#{row_index}, #{column_index}"
       inner_len = directors_database[row_index][column_index].length
+      puts "\tCoordinate [#{coord}] points to an #{directors_database[row_index][column_index].class} of length #{inner_len}"
+      column_index += 1
     row_index += 1
   end
 end
