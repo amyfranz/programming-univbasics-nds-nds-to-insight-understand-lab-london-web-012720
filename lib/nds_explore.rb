01b222e6b11e2d3afb35628c62bad0c9b76422cf
Amy Franz <amy.franz@gmail.com>
2020-01-06 09:32:08 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -18,7 +18,7 @@ def print_first_directors_movie_titles
   i = 0
   while i < directors_database[i].length do
     if directors_database[i][:name] = "Stephen Spielberg"
-      puts i
+      puts directors_database[i][:name]
       m = i
     end
     i += 1
