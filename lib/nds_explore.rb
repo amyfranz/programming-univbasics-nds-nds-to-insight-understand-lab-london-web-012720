db310d3a23f2f1138c1191f104957aa7188d0b0a
Amy Franz <amy.franz@gmail.com>
2020-01-06 09:24:22 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,6 +10,7 @@ def print_first_directors_movie_titles
   i = 0
   while i < directors_database[0][:movies].length do
     puts directors_database[0][:movies][i]
+    puts i
     i += 1 
   end
 end
